5c370e3c7eab7d75de0b3cdf349e4780091700a8
yschin <cys1991@gmail.com>
2013-07-22 23:18:30 +1000

    Cleaned up code

--- moduleA/victraffic.py
+++ moduleA/victraffic.py
@@ -3,52 +3,23 @@
 # for use with VicRoads-VicTraffic Website
 ################################################################################
 import urllib
-import time
 
 from HTMLParser import HTMLParser
-
-# Important HTML Tags
-#LINK = "href"
-#TITLE = "title"
-
-# Custom Omissions
-#blocked_urls = ["timetables/", "disruptions/", "tickets/myki/"]
-
-# Keywords 
-
-# Regular Expressions
 
 # HTML Parser
 class victrafficHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == "li":
-            print attrs[0][1]
+            print "\n", str.upper(attrs[0][1])
 
     def handle_endtag(self, tag):
         return
 
     def handle_data(self, data):
-        print data
-
-# Data Processing Function
-"""
-Function: build_output
-PARAM:
-  @ victrafficdata, DICTIONARY data set.
-  @ key, what is your data part of?
-  @ data, concatenates your current data to lists of data existing for the given
-          key.
-
-RETURN:
-  [currently] prints formated output according to what you have supplied.
-"""
-def build_output(victrafficdata, key, data):
-  while(True):
-    try:
-      victrafficdata[key] = victrafficdata[key].append(data)
-      break
-    except KeyError:
-      victrafficdata[key] = []  
+        if ("=" in data):
+            return
+        elif str.strip(data):
+            print data
       
 # Main Module
 page = urllib.urlopen("http://traffic.vicroads.vic.gov.au/nojs/")
