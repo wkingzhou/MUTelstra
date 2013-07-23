9ae824b8c12105ab7912918fbfffdbf910eb8fce
Renlord <renlord.yang@gmail.com>
2013-07-23 19:09:36 +1000

    jj

--- moduleA/ptv.py
+++ moduleA/ptv.py
@@ -1,6 +1,7 @@
 # UoM Telstra M2M Challenge
 # HMTL Information Extractor
 # for use with PTV Website
+# Renlord Y.
 ################################################################################
 import urllib
 import time
@@ -15,29 +16,31 @@ TITLE = "title"
 blocked_urls = ["timetables/", "disruptions/", "tickets/myki/"]
 
 # Keywords 
+transport = ['trains', 'tram', 'buses']
 
 # Regular Expressions
 
+
 # HTML Parser
 class ptvHTMLParser(HTMLParser):
-  def handle_starttag(self, tag, attrs):
-    blocking = False
-    if tag == "a" and attrs[0][0] == TITLE and attrs[1][0] == LINK:
-      for url in blocked_urls:
-        if url == attrs[1][1]:
-          blocking = True
-          break
-      if not blocking:
-        #TEMPORARY
-        print attrs[0][1]
-        print attrs[1][1]
+    def handle_starttag(self, tag, attrs):
+        blocking = False
+        if tag == "a" and attrs[0][0] == TITLE and attrs[1][0] == LINK:
+            for url in blocked_urls:
+                if url == attrs[1][1]:
+                    blocking = True
+                    break
+            if not blocking:
+                #TEMPORARY
+                print attrs[0][1]
+                print attrs[1][1]
 
-  def handle_endtag(self, tag):
-    return
+    def handle_endtag(self, tag):
+        return
 
-  def handle_data(self, data):
-    if toggle_data == True:
-      print data
+    def handle_data(self, data):
+        if toggle_data == True:
+            print data
 
 # Data Processing Function
 """
@@ -52,12 +55,37 @@ RETURN:
   [currently] prints formated output according to what you have supplied.
 """
 def build_output(ptvdata, key, data):
-  while(True):
-    try:
-      ptvdata[key] = ptvdata[key].append(data)
-      break
-    except KeyError:
-      ptvdata[key] = []  
+    while(True):
+        try:
+            ptvdata[key] = ptvdata[key].append(data)
+            break
+        except KeyError:
+            ptvdata[key] = []  
+
+"""
+dateHandler(string)
+PARAM:
+    @string, string TYPE only literal date formats.
+
+RETURN:
+    disruption duration.
+"""
+def dateHandler(string):
+    words = string.split()
+    for word in words:
+        try:
+            day = int(word)
+        except ValueError:
+            if word in months:
+                date.append((day, word))
+            else:
+                if word == '-':
+
+                if word == 'and':
+
+                
+                
+                
       
 # Main Module
 page = urllib.urlopen("http://ptv.vic.gov.au/disruptions/")
@@ -67,9 +95,50 @@ parser = ptvHTMLParser()
 
 ptvdata = {}
 
+
+
 toggle_data = False
 
 parser.feed(page)
+
+# Trains 
+def mTrain(string, url):
+    # train disruption keywords
+    disruption = ['replacing', 'closed', 'delays', 'disrupted']
+    
+    # train line services
+    tLines = ['cranbourne', 'pakenham', 'werribee', 'williamstown', \
+        'sandringham', 'frankston', 'craigieburn', 'alamein', \
+        'belgrave', 'lilydale', 'glen waverley']
+
+    # months
+    months = ['january', 'february', 'march', 'april', 'may', 'june', \
+                'july', 'august', 'september', 'october', 'november', \
+                'december']
+    
+    #time keys
+    time = ['am', 'pm']
+
+    affected_lines = []
+    affected_dates = []
+
+    words = string.split()
+
+    for word in words:
+        if word.lower() in tLines:
+            affected_lines.append(word)
+        if word.lower() in months:
+            affected_dates = 
+        
+    
+    
+    
+    
+# Tram
+def tram():
+
+# Buses
+def bus():
 
 
 
